9937ba8d1540f82d4d2af213bc38e9058858d25b
Abdul Rub <arub0419@gmail.com>
2023-02-03 23:43:04 +0530

    tour-page

--- client/src/pages/Tours.jsx
+++ client/src/pages/Tours.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import CommonSection from '../shared/CommonSection'
+import "../styles/tour.css"
+import TourCard from "../shared/TourCard"
+import SearchBar from "../shared/SearchBar"
+import Newsletter from "../shared/Newsletter"
+import tourData from "../assets/data/tours"
+import { Container, Row } from 'reactstrap'
 
 const Tours = () => {
   return (
-    <div>Tours Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora repudiandae ducimus mollitia repellat est rem ullam eligendi, praesentium aliquam temporibus, minima quia neque, quis quasi eum voluptatum recusandae cum assumenda.</div>
+    <>
+    <CommonSection title={"All Tours"}/>
+    <section>
+      <Container>
+     <Row>
+      <SearchBar/>
+     </Row>
+      </Container>
+    </section>
+    </>
   )
 }
 
